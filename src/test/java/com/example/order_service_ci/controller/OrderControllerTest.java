b475ec4878a1aa6686634f4b25f98d33e19eda4d
Poojitha Chilukuri <chpoojitha2002@gmail.com>
2026-02-09 19:10:19 +0530

    Add new test to test webhook trigger

--- src/test/java/com/example/order_service_ci/controller/OrderControllerTest.java
+++ src/test/java/com/example/order_service_ci/controller/OrderControllerTest.java
@@ -32,21 +32,7 @@ class OrderControllerTest {
                         .content(json))
                 .andExpect(status().isOk());
     }
-    @Test
-    void shouldRejectOrderWithNegativeQuantity() throws Exception {
-        // JSON with invalid quantity
-        String json = """
-    {
-      "item": "Book",
-      "quantity": -5
-    }
-    """;
 
-        mockMvc.perform(post("/orders")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isBadRequest());  // Expect HTTP 400
-    }
 
 
 }
